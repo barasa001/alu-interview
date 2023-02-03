120dcf803b1e616145ce6a709e1d6b25ee87e47d
barasa001 <s.wanyonyi@alustudent.com>
2023-02-03 06:03:21 -0800

    test file

---
+++ minimum_operations/0-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Main filr for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
