71e47250688aff469c5b18d4ff1b4b133f5e6ae5
barasa254 <101549153+barasa001@users.noreply.github.com>
2023-02-06 09:59:22 +0300

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""calculates the fewest number of operations needed
-to result in exactly n H characters"""
+"""
+Minimum Operations
+"""
+
 
 def minOperations(n):
-    if n <= 0:
+    """
+    Given a single-character text editor with only the character 'H', calculate the minimum number of operations 
+    needed to transform the editor into one that has `n` 'H' characters.
+    
+    Args:
+    n: an integer representing the desired number of 'H' characters in the editor.
+    
+    Returns:
+    An integer representing the minimum number of operations required to achieve `n` 'H' characters.
+    """
+    
+    if n <= 1:
         return 0
+    Argmnt, evn, Operations = n, 2, 0
 
-    ans = 0
-    i = 2
-    while i * 1 <= n:
-        while n % i == 0:
-            ans += 1
-            n /= i
-        i += 1
-    if n > 1:
-        res += 1
-    return ans
-
+    while Argmnt > 1:
+        if Argmnt % evn == 0:
+            Argmnt = Argmnt / evn
+            Operations = Operations + evn
+        else:
+            evn += 1
+    return Operations
