63d09cfbd4ad530954ddae2c60418340fcf2de99
barasa001 <s.wanyonyi@alustudent.com>
2023-02-05 23:08:49 -0800

    correction

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -9,15 +9,15 @@ def minOperations(n):
     Given a single-character text editor with only the character 'H',
     calculate the minimum number of operations
     needed to transform the editor into one that has `n` 'H' characters.
-    
+
     Args:
-    n: an integer representing the desired number of 'H' characters in the editor.
-    
+    n: an integer representing the desired number
+    of 'H' characters in the editor.
+
     Returns:
     An integer representing the minimum number of
     operations required to achieve `n` 'H' characters.
     """
-    
     if n <= 1:
         return 0
     Argmnt, evn, Operations = n, 2, 0
